4c4b3804031facacf35a3596ecf4bb422a869ef2
Sivatharan <sivatharanoff97@gmail.com>
2026-01-30 08:27:13 +0530

    show chat screen based on authentication state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -23,7 +25,16 @@ class App extends StatelessWidget {
           seedColor: const Color(0x0017CE92),
         ),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/screens/chat.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Convo Space',
+          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: const Center(
+        child: Text('Logged in...'),
+      ),
+    );
+  }
+}
